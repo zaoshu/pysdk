8cd331bf2370e0a037df6b7b91c92d65f15d953e
BuleAnt <cheng1483x@gmail.com>
2017-08-03 18:25:21 +0800

    circleci

--- zaoshu/zaoshu_test.py
+++ zaoshu/zaoshu_test.py
@@ -107,11 +107,17 @@ class TestInstance(unittest.TestCase):
 
     def test_1_list(self):
         """测试获取用户的爬虫实例列表"""
-        instance_list_response = self.instance.list()
+        instance_list_response = self.instance.
+        ()
+
+
+
+
+
         self.assertEqual(instance_list_response.status_code, 200)
         self.instance_id = instance_list_response.json()['data'][0]['id']
 
-    def test_2_item(self):
+    def                     test_2_item(self):
         """测试获取单个爬虫实例实例"""
         self.assertNotEqual(self.instance_id, '')
         instance_item_response = self.instance.item(self.instance_id)
